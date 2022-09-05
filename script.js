39fe333172c1191986bad3434b5558029c7df8ca
TIMUR ZAKIROV <DAMIROWIC@GMAIL.COM>
2022-09-05 12:13:38 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const adjective1 = ['Big', 'Bold', 'Stupid', 'Angry', 'Valiant'];
+const adjective2 = ['sneaky', 'dirty', 'solid', 'pushy'];
+const  noun = ['dwarf', 'knight', 'jerk', 'пес'];
+let sentence = 'You are ';
+let x = Math.floor(Math.random() * adjective1.length);
+sentence += adjective1[x];
+sentence += ' ';
+let y = Math.floor(Math.random() * adjective2.length);
+sentence += adjective1[y];
+sentence += ' ';
